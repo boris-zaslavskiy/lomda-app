d0a7b918dd48b96fd4a8bbd7eb7f89cfa51b611b
Alina <alinahom@me.com>
2021-07-19 08:55:22 +0300

    'main'

--- src/Pages/Main/Main.js
+++ src/Pages/Main/Main.js
@@ -1,16 +1,14 @@
 import React from 'react';
-<<<<<<< HEAD
 import styles from './Main.module.css';
-import global from '../../Global/Models/Global.module.css';
-=======
+
 //import styles from './Main.module.css';
 import global from '../../Global/Modules/Global.module.css';
->>>>>>> upstream/main
+
 import Header from "../../Global/Navigation/Header/Header";
 import {Container} from "react-bootstrap";
 import {WelcomeSection} from "../../Global/Components/Sections/WelcomeSection/WelcomeSection";
 import {UserCard} from "../../Global/Components/UserCard/UserCard";
-import {TitleH2} from "../../Global/Components/Texts/Headers/TitleH2/TitleH2";
+
 
 
 const Main = () => {
@@ -18,13 +16,12 @@ const Main = () => {
         <Container fluid className={global.Wrapper}>
             <Header/>
             <WelcomeSection pageName='main' addBlock='form'/>
-<<<<<<< HEAD
+
             <div className={styles.block}>
                 <UserCard/>
             </div>
 
-=======
->>>>>>> upstream/main
+
         </Container>
     );
 };
