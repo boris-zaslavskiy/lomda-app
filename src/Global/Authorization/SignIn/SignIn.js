7236c8e48dd059a336e77d13506beec5b664f4ab
Jassy369 <hechhocky@gmail.com>
2021-07-31 20:01:55 +0300

    Authorization forms

--- src/Global/Authorization/SignIn/SignIn.js
+++ src/Global/Authorization/SignIn/SignIn.js
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, {useState} from 'react';
+import styles from "./SignIn.module.css";
+import RInput from "../RInput/RInput";
+import {Form} from "react-bootstrap";
 
 const SignIn = () => {
+
+    const [formState, setFormState] = useState({
+        teacher:true,
+        student:false
+    })
+
+    const render = () =>{
+        if (formState.teacher === true){
+            return(
+            <div className={styles.signIn}>
+                <label className={`${styles.label} ${styles.active}`} onClick={()=>{
+                    setFormState({...formState , teacher:true, student: false})
+                }}>I'm teacher <i className={`fa fa-check ${styles.labelCheck}`}/></label>
+                <label className={`${styles.label}`} onClick={()=>{
+                    setFormState({...formState , teacher:false, student: true})
+                }}>I'm student</label>
+                <Form className={styles.formWrapper}>
+                    <RInput type='first-name'/>
+                    <RInput type='last-name'/>
+                    <RInput type='email' />
+                    <RInput type='password'/>
+                    <RInput type='confirm-password'/>
+                    <div className={`w-100 d-flex`}>
+                    <button className={styles.subBtn}>Submit</button>
+                    </div>
+                </Form>
+            </div>
+            )}else{
+                return (
+                    <div className={styles.signIn}>
+                        <label className={`${styles.label}`} onClick={()=>{
+                            setFormState({...formState , teacher:true, student: false})
+                        }}>I'm teacher</label>
+                        <label className={`${styles.label} ${styles.active}`} onClick={()=>{
+                            setFormState({...formState , teacher:false, student: true})
+                        }}>I'm student <i className={`fa fa-check ${styles.labelCheck}`}/></label>
+                        <Form className={styles.formWrapper}>
+                            <RInput type='first-name'/>
+                            <RInput type='last-name'/>
+                            <RInput type='email'/>
+                            <RInput type='select'/>
+                            <RInput type='password'/>
+                            <RInput type='confirm-password'/>
+                            <div className={`w-100 d-flex`}>
+                                <button className={styles.subBtn} type={`submit`}>Submit</button>
+                            </div>
+                        </Form>
+                    </div>
+                )
+            }
+        }
+
+
     return (
-        <div>
-            
-        </div>
+        render()
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 import store from "./Store/store";
+import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
